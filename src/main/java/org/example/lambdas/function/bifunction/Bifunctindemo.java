b1fdf598a906b8831c69cfc52a54e8bb7d3f8647
aaa <aaa@aaas-MacBook-Pro.local>
2024-08-20 20:48:23 +0530

    bifunction demo

--- src/main/java/org/example/lambdas/function/bifunction/Bifunctindemo.java
+++ src/main/java/org/example/lambdas/function/bifunction/Bifunctindemo.java
@@ -1,4 +1,18 @@
 package org.example.lambdas.function.bifunction;
 
+import java.util.function.BiFunction;
+
+/**
+ * BiFunction<T,U,R>
+ * The BiFunction<T, U, R> is similar to Function<T, R> interface;
+ * the only difference is that the BiFunction interface takes in two parameters and
+ * returns an output.
+ *
+ * Below is the list of methods in the BiFunction interface.
+ */
 public class Bifunctindemo {
+    public static void main(String[] args) {
+        BiFunction<Integer, Integer, Integer> sum = (a, b) -> a + b;
+        System.out.println("sum of the two numbers are "+sum.apply(22,32));
+    }
 }

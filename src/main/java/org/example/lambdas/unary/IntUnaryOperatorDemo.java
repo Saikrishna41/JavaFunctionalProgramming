3f9d70deb077653143f17b7df08abca17f266940
aaa <aaa@aaas-MacBook-Pro.local>
2024-08-21 11:04:33 +0530

    unary operator and intunaryoperator

---
+++ src/main/java/org/example/lambdas/unary/IntUnaryOperatorDemo.java
@@ -0,0 +1,11 @@
+package org.example.lambdas.unary;
+
+import java.util.function.IntUnaryOperator;
+
+public class IntUnaryOperatorDemo {
+    public static void main(String[] args) {
+        IntUnaryOperator operator = (a) -> a * a;
+        System.out.println("The product is "+operator.applyAsInt(23));
+
+    }
+}
---
+++ src/main/java/org/example/lambdas/unary/UnaryOperatorDemo.java
@@ -0,0 +1,49 @@
+package org.example.lambdas.unary;
+
+import org.example.lambdas.Person;
+
+import java.util.function.Function;
+import java.util.function.UnaryOperator;
+
+class Person2 {
+    private String  name;
+    private int age;
+
+    public Person2(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Person2() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
+public class UnaryOperatorDemo {
+    public static void main(String[] args) {
+        Person2 person = new Person2();
+        UnaryOperator<Person2> operator = person1 -> {
+            person1.setName("Sai");
+            person1.setAge(33);
+            return person1;
+        };
+
+        operator.apply(person);
+
+        System.out.println("Person name "+person.getName()+ " and age is "+person.getAge());
+    }
+}
